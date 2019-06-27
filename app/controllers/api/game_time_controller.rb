a98cbe15cf5cd79e6a52a3c2e168599a0a3fc999
meeae <meeaeking@gmail.com>
2019-06-26 19:32:12 -0500

    modify logic to print out the name and message if the name begins with a

--- app/controllers/api/game_time_controller.rb
+++ app/controllers/api/game_time_controller.rb
@@ -1,6 +1,17 @@
 class Api::GameTimeController < ApplicationController
   def letter
-  
+    name = params["name"]
+    first_character = params["name"][0]
+    if first_character == "a"
+      @message = "The first letter of your name is a"
+    else
+      @message = "Your name is #{name}"
+    end
+
     render 'name.json.jb'
   end
+
+  # def number
+  #   if param["number"] == rand(1..100)
+  # end
 end
